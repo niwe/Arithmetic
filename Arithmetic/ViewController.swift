f51862ee981105ff1d8d74d3a78dfb24c89b5808
Nicklas <Nicklas@stardust.local>
2017-01-29 10:34:58 +0100

    Basic layout

--- Arithmetic/ViewController.swift
+++ Arithmetic/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UICollectionViewController
+{
+    let array = ["item1", "item2", "item3"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return array.count
+    }
+    
+    
+    
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customcell", for: indexPath) as UICollectionViewCell
+        
+        return cell
+    }
 }
 
